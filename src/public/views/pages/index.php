52c6692c47257c827cb074540f6bc9ae872a49cb
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-10-04 16:09:16 +0700

    add homepage template

--- src/public/views/pages/index.php
+++ src/public/views/pages/index.php
@@ -1,4 +1,25 @@
+<!DOCTYPE html>
+<html>
 
-<h1>
-    hi
-</h1>
+<head>
+    <title>Welcome to Sevenify</title>
+    <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+</head>
+
+<body>
+    <section id="section-album">
+        <h2>Your Albums</h2>
+    </section>
+
+    <section id="section-playlist">
+        <h2>Your Playlists</h2>
+    </section>
+
+    <section id="section-music">
+        <h2>Your Musics</h2>
+    </section>
+
+    <script src="src\public\javascript\homepage.js"></script>
+</body>
+
+</html>
