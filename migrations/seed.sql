29368ef1976b0f45ae8ecd4cebd30d6ab6ee2774
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-10-05 16:28:35 +0700

    dev: fix bug sql

--- migrations/seed.sql
+++ migrations/seed.sql
@@ -1,9 +1,7 @@
 -- Create the database if it doesn't exist (if applicable)
 CREATE DATABASE IF NOT EXISTS sevenify;
-
 -- Use the database
 USE sevenify;
-
 -- Create tables
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -11,7 +9,6 @@ CREATE TABLE IF NOT EXISTS users (
     user_password VARCHAR(255) NOT NULL,
     role ENUM('user', 'admin') NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS albums (
     album_id INT AUTO_INCREMENT PRIMARY KEY,
     album_name VARCHAR(255) NOT NULL,
@@ -19,7 +16,6 @@ CREATE TABLE IF NOT EXISTS albums (
     album_cover_path VARCHAR(255),
     FOREIGN KEY (album_owner) REFERENCES users(user_id)
 );
-
 CREATE TABLE IF NOT EXISTS playlists (
     playlist_id INT AUTO_INCREMENT PRIMARY KEY,
     playlist_name VARCHAR(255) NOT NULL,
@@ -27,15 +23,13 @@ CREATE TABLE IF NOT EXISTS playlists (
     playlist_cover_path VARCHAR(255),
     FOREIGN KEY (playlist_owner) REFERENCES users(user_id)
 );
-
 CREATE TABLE IF NOT EXISTS music (
     music_id INT AUTO_INCREMENT PRIMARY KEY,
     music_name VARCHAR(255) NOT NULL,
     music_owner INT NOT NULL,
     music_genre VARCHAR(255),
-    FOREIGN KEY (music_owner) REFERENCES users(user_id),
+    FOREIGN KEY (music_owner) REFERENCES users(user_id)
 );
-
 CREATE TABLE IF NOT EXISTS playlist_music (
     music_id INT NOT NULL,
     playlist_id INT NOT NULL,
@@ -43,7 +37,6 @@ CREATE TABLE IF NOT EXISTS playlist_music (
     FOREIGN KEY (music_id) REFERENCES music(music_id),
     FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id)
 );
-
 CREATE TABLE IF NOT EXISTS album_music (
     music_id INT NOT NULL,
     album_id INT NOT NULL,
