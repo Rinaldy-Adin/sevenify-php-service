ecbd904c3ca102c919b90688912c60c41beb386c
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-10-08 06:02:44 +0700

    dev : add playlist item

--- src/public/views/pages/index.php
+++ src/public/views/pages/index.php
@@ -1,17 +1,18 @@
 <?php
 require_once ROOT_DIR . 'models/albumModel.php';
 require_once ROOT_DIR . 'models/musicModel.php';
+require_once ROOT_DIR . 'models/playlistModel.php';
 require_once ROOT_DIR . 'repositories/musicRepository.php';
 require_once ROOT_DIR . 'repositories/albumRepository.php';
-require_once ROOT_DIR . 'public/views/components/musicItem.php';
 require_once ROOT_DIR . 'public/views/components/musicList.php';
-require_once ROOT_DIR . 'public/views/components/albumItem.php';
 require_once ROOT_DIR . 'public/views/components/albumList.php';
+require_once ROOT_DIR . 'public/views/components/playlistItem.php';
 
 use models\MusicModel;
 use repositories\MusicRepository;
 use models\AlbumModel;
 use repositories\AlbumRepository;
+use models\PlaylistModel;
 
 $musicRepository = new MusicRepository();
 $albumRepository = new AlbumRepository();
@@ -24,10 +25,9 @@ $albumRepository = new AlbumRepository();
     <meta charset="UTF-8">
     <link rel="stylesheet" href="public/styles/global.css">
     <link rel="stylesheet" href="public/styles/music-bar.css">
-    <link rel="stylesheet" href="public/styles/music-item.css">
     <link rel="stylesheet" href="public/styles/music-list.css">
-    <link rel="stylesheet" href="public/styles/album-item.css">
     <link rel="stylesheet" href="public/styles/album-list.css">
+    <link rel="stylesheet" href="public/styles/playlist-item.css">
     <title>Sevenify</title>
 </head>
 
@@ -40,6 +40,11 @@ $albumRepository = new AlbumRepository();
 
     <section id="section-playlist">
         <h2>Your Playlists</h2>
+        <?php
+            $playlist = new PlaylistModel(1, "Playlist1", 1, "/public/assets/placeholders/playlist-placeholder.png");
+            echo playlistItem($playlist);
+        ?>
+
     </section>
 
     <section id="section-music">
@@ -48,7 +53,6 @@ $albumRepository = new AlbumRepository();
 
     <script src="public/javascript/adios.js"></script>
     <script src="public/javascript/music-bar.js"></script>
-    <script src="/public/javascript/music-item.js"></script>
     <script src="/public/javascript/music-list.js"></script>
     <script src="/public/javascript/album-list.js"></script>
 
