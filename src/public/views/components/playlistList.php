44a4059e8a47872ebe92d433ca997e5b76ee99a3
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-10-08 07:16:31 +0700

    fix: playlist display on home

---
+++ src/public/views/components/playlistList.php
@@ -0,0 +1,3 @@
+<h2>Your Playlist</h2>
+<div class="playlist-slider" id="playlist-slider"></div>
+<div id="pagination-playlist"></div>
--- src/public/views/pages/index.php
+++ src/public/views/pages/index.php
@@ -6,16 +6,18 @@ require_once ROOT_DIR . 'repositories/musicRepository.php';
 require_once ROOT_DIR . 'repositories/albumRepository.php';
 require_once ROOT_DIR . 'public/views/components/musicList.php';
 require_once ROOT_DIR . 'public/views/components/albumList.php';
-require_once ROOT_DIR . 'public/views/components/playlistItem.php';
+require_once ROOT_DIR . 'public/views/components/playlistList.php';
 
 use models\MusicModel;
 use repositories\MusicRepository;
 use models\AlbumModel;
 use repositories\AlbumRepository;
 use models\PlaylistModel;
+use repositories\PlaylistRepository;
 
 $musicRepository = new MusicRepository();
 $albumRepository = new AlbumRepository();
+$playlistRepository = new PlaylistRepository();
 ?>
 
 <!DOCTYPE html>
@@ -27,7 +29,7 @@ $albumRepository = new AlbumRepository();
     <link rel="stylesheet" href="public/styles/music-bar.css">
     <link rel="stylesheet" href="public/styles/music-list.css">
     <link rel="stylesheet" href="public/styles/album-list.css">
-    <link rel="stylesheet" href="public/styles/playlist-item.css">
+    <link rel="stylesheet" href="public/styles/playlist-list.css">
     <title>Sevenify</title>
 </head>
 
@@ -39,12 +41,7 @@ $albumRepository = new AlbumRepository();
     </section>
 
     <section id="section-playlist">
-        <h2>Your Playlists</h2>
-        <?php
-            $playlist = new PlaylistModel(1, "Playlist1", 1, "/public/assets/placeholders/playlist-placeholder.png");
-            echo playlistItem($playlist);
-        ?>
-
+        <?php require ROOT_DIR . '/public/views/components/playlistList.php'; ?>
     </section>
 
     <section id="section-music">
@@ -55,6 +52,7 @@ $albumRepository = new AlbumRepository();
     <script src="public/javascript/music-bar.js"></script>
     <script src="/public/javascript/music-list.js"></script>
     <script src="/public/javascript/album-list.js"></script>
+    <script src="/public/javascript/playlist-list.js"></script>
 
 </body>
 
