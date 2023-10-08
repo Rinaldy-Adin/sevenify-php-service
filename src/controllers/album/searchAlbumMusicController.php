d346fa5be51e914e93b40242197221a37d5b8edb
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-10-08 18:33:54 +0700

    add search album music controller

---
+++ src/controllers/album/searchAlbumMusicController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace controllers\album;
+
+require_once ROOT_DIR . 'common/response.php';
+require_once ROOT_DIR . 'common/dto/musicWithArtistNameDTO.php';
+require_once ROOT_DIR . 'services/musicService.php';
+
+use common\dto\MusicWithArtistNameDTO;
+use common\Response;
+use services\MusicService;
+
+class SearchAlbumMusicController
+{
+    function get(): string
+    {
+        $albumId = isset($_GET['albumId']) ? urldecode($_GET['albumId']) : -1;
+        $page = isset($_GET['page']) ? urldecode($_GET['page']) : 1;
+
+        [$musicDTOs, $pageCount] = (new MusicService())->getByAlbumId($albumId, $page);
+        $searchResult = array_map(fn(MusicWithArtistNameDTO $dto) => $dto->toDTOArray(), $musicDTOs);
+        return (new Response(['result' => $searchResult, 'page-count' => $pageCount]))->httpResponse();
+    }
+}
